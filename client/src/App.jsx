e72a5bb798005f3b40287860b57be527d9ec04f2
yves40 <yves.toubhans@free.fr>
2026-05-03 18:48:37 +0200

    Static data to test only the vite server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+/* eslint-disable no-unused-vars */
 
 import { useState, useEffect } from 'react'
-import './compiled.css'
+import './index.css'
 
 
 function App() {
   const [apiData, setApiData] = useState([])
+  const [fakedata, setFakedata] = useState(
+    [
+      { name: 'Ken Tyrell', age: 30, email: 'ken.tyrell@example.com' },
+      { name: 'Jackie Stewart', age: 30, email: 'jackie.stewart@example.com' },
+      { name: 'Carlos Reuteman', age: 30, email: 'carlos.reuteman@example.com' },
+      { name: 'Mario Andretti', age: 30, email: 'mario.andretti@example.com' },
+    ]
+  )  
   
   const fetchApi = async () => {
     try {
@@ -18,20 +27,19 @@ function App() {
   }
 
   useEffect(() => {
-    fetchApi()
   }, [])  // initial render
 
   return (
-    <>
       <div className='min-h-screen w-full bg-gray-100 flex items-center justify-center flex-col gap-10'>
-        <h2 className=' text-2xl font-bold italic text-green-800'>Fruits from API:</h2>
-        <ul className=' text-red-600 text-2xl font-semibold'>
-          {apiData.map((fruit, index) => (
-            <li key={index}>{fruit}</li>
+        <h2 className=' text-2xl font-bold italic text-green-800'>F1 from static:</h2>
+        <ul className='text-gray-600 text-2xl font-semibold'>
+          {fakedata.map((person, index) => (
+            <li key={index} className='hover:text-blue-300'>
+              {person.email} - {person.name}
+            </li>
           ))}
         </ul>
       </div>
-    </>
   )
 }
 
